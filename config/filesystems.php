bde79c1b3a223ebef35331ca08be5ca8f9d1c686
mihail B <beltyukov-mihail@mail.ru>
2026-01-01 19:11:44 +0500

    какие-то изменения

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,9 +55,15 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true),
             'throw' => false,
             'report' => false,
+            'options' => [
+                'verify' => false,
+            ],
+            'http' => [
+                'verify' => false,
+            ]
         ],
 
     ],
